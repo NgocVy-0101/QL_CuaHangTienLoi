86e776b9a18f37be13844b232fe3e7cdea8b9c19
NgocVy-0101 <ngocvy.5267@gmail.com>
2024-12-01 02:38:35 +0700

    Update KhachHangDAL.cs

--- Nhom01_QuanLyCuaHangTienLoi/QuanLiCuaHangTienLoi_DAL/KhachHangDAL.cs
+++ Nhom01_QuanLyCuaHangTienLoi/QuanLiCuaHangTienLoi_DAL/KhachHangDAL.cs
@@ -11,17 +11,19 @@ namespace QuanLiCuaHangTienLoi_DAL
 {
     public class KhachHangDAL
     {
+       
         SqlConnection conn;
         DataSet ds = new DataSet();
         SqlDataAdapter adap;
+        SqlCommand cmd;
         public KhachHangDAL()
         {
-            string s = @"Data Source=LAPTOP-SF87IO3T\PHAMVU;Initial Catalog=QLCHTL;Integrated Security=True";
+            string s = @"Data Source=LAPTOP-LDL5LP51\SQLEXPRESS;Initial Catalog=QLCHTL;Integrated Security=True";
             conn = new SqlConnection(s);
         }
-        public List<KhacHangDTO> KhachHang()
+        public List<KhachHangDTO> KhachHang()
         {
-            List<KhacHangDTO> k = new List<KhacHangDTO>();
+            List<KhachHangDTO> k = new List<KhachHangDTO>();
             string sql = "select * from KhachHang";
             adap = new SqlDataAdapter(sql, conn);
             adap.Fill(ds, "KhachHang");
@@ -37,11 +39,82 @@ namespace QuanLiCuaHangTienLoi_DAL
                 string t = i["TenKhach"].ToString();
                 string dt = i["DienThoai"].ToString();
                 int di = (int)i["Diem"];
-                KhacHangDTO o = new KhacHangDTO(m, t, dt, di);
+                KhachHangDTO o = new KhachHangDTO(m, t, dt, di);
                 k.Add(o);
             }
 
             return k;
+        }
+
+        public bool ThemKhachHang(KhachHangDTO kh)
+        {
+            try
+            {
+                string sql = "INSERT INTO KhachHang (MaKhach, TenKhach, DienThoai, Diem) VALUES (@MaKhach, @TenKhach, @DienThoai, @Diem)";
+                cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@MaKhach", kh.MaKhach);
+                cmd.Parameters.AddWithValue("@TenKhach", kh.TenKhach);
+                cmd.Parameters.AddWithValue("@DienThoai", kh.DienThoai);
+                cmd.Parameters.AddWithValue("@Diem", kh.Diem);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        // Cập nhật khách hàng
+        public bool CapNhatKhachHang(KhachHangDTO kh)
+        {
+            try
+            {
+                string sql = "UPDATE KhachHang SET TenKhach = @TenKhach, DienThoai = @DienThoai, Diem = @Diem WHERE MaKhach = @MaKhach";
+                cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@MaKhach", kh.MaKhach);
+                cmd.Parameters.AddWithValue("@TenKhach", kh.TenKhach);
+                cmd.Parameters.AddWithValue("@DienThoai", kh.DienThoai);
+                cmd.Parameters.AddWithValue("@Diem", kh.Diem);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        // Xóa khách hàng
+        public bool XoaKhachHang(string maKhach)
+        {
+            try
+            {
+                string sql = "DELETE FROM KhachHang WHERE MaKhach = @MaKhach";
+                cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@MaKhach", maKhach);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                conn.Close();
+            }
         }
     }
 }
