0b4a6f050256b3db459742212aa07957ef87cffc
NgocVy-0101 <ngocvy.5267@gmail.com>
2024-12-01 02:36:37 +0700

    Update NhanVienBLL.cs

--- Nhom01_QuanLyCuaHangTienLoi/QuanLiCuaHangTienLoi_BLL/NhanVienBLL.cs
+++ Nhom01_QuanLyCuaHangTienLoi/QuanLiCuaHangTienLoi_BLL/NhanVienBLL.cs
@@ -9,11 +9,28 @@ using QuanLiCuaHangTienLoi_DTO;
 namespace QuanLiCuaHangTienLoi_BLL
 {
     public class NhanVienBLL
-    {
-        NhanVienDAL nv = new NhanVienDAL();
+    {NhanVienDAL dal = new NhanVienDAL();
         public List<NhanVienDTO> NhanVien()
         {
-            return nv.NhanVien();
+            return dal.NhanVien();
+        }
+
+        public bool ThemNhanVien(NhanVienDTO nvDTO)
+        {
+            NhanVienDAL dal = new NhanVienDAL();
+            return dal.ThemNhanVien(nvDTO);
+        }
+
+        public bool CapNhatNhanVien(NhanVienDTO nvDTO)
+        {
+            NhanVienDAL dal = new NhanVienDAL();
+            return dal.CapNhatNhanVien(nvDTO);
+        }
+
+        public bool XoaNhanVien(string maNhanVien)
+        {
+            NhanVienDAL dal = new NhanVienDAL();
+            return dal.XoaNhanVien(maNhanVien);
         }
     }
 }
