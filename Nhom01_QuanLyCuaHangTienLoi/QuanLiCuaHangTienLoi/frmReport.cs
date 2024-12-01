d93da60fa6663e8b79e0a50173bbe08d11216725
NgocVy-0101 <ngocvy.5267@gmail.com>
2024-12-02 06:40:09 +0700

    Add files via upload

---
+++ Nhom01_QuanLyCuaHangTienLoi/QuanLiCuaHangTienLoi/frmReport.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using CrystalDecisions.CrystalReports.Engine;
+
+namespace QuanLiCuaHangTienLoi
+{
+    public partial class frmReport : Form
+    {
+        public frmReport()
+        {
+            InitializeComponent();
+        }
+
+        public void LoadReport(string reportPath, DataTable data)
+        {
+            try
+            {
+                ReportDocument reportDocument = new ReportDocument();
+                reportDocument.Load(reportPath);
+                reportDocument.SetDataSource(data);
+
+                // Thiết lập thông tin đăng nhập cho cơ sở dữ liệu (nếu cần)
+                reportDocument.SetDatabaseLogon("sa", "010104");
+
+                crystalReportViewer1.ReportSource = reportDocument;
+                crystalReportViewer1.Refresh();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Có lỗi xảy ra khi tải báo cáo: " + ex.Message + "\n" + ex.StackTrace);
+            }
+        }
+
+        private void frmReport_Load(object sender, EventArgs e)
+        {
+            // Không làm gì ở đây, vì chúng ta sẽ gọi LoadReport từ form khác.
+        }
+    }
+}
